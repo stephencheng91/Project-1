4322cbf93718a052aed0ab104fdbbca0ad9d3ca3
Casey <caseyclinga@gmail.com>
2019-04-13 13:19:50 -0700

    3.14 update

--- assets/app.js
+++ assets/app.js
@@ -18,6 +18,10 @@
  var age = 0;
  var city = "";
  var gender = "";
+ var politics = "";
+ var religion = "";
+ var computer = "";
+ var guns = "";
 
  $("#add-user-btn").click(function(event) {
    
@@ -31,16 +35,31 @@
     } else {
         gender = "female"
     }
-
+    religion = $("#religion-input").val();
+    computer = $("#computer-input").val();
+    guns = $("#guns-input").val();
 
     database.ref().push({
         name: name,
         age: age,
         city: city,
-        gender: gender
+        gender: gender,
+        religion: religion,
+        computer: computer,
+        guns: guns
     })
 
+    localStorage.clear();
+    localStorage.setItem('selectedName', name);
+    localStorage.setItem('selectedAge', age);
+    localStorage.setItem('selectedCity', city);
+    localStorage.setItem('selectedGender', gender);
+    localStorage.setItem('selectedReligion', religion);
+    localStorage.setItem('selectedComputer', computer);
+    localStorage.setItem('selectedGuns', guns );
+
     database.ref().orderByChild("city").equalTo(city).on("child_added", function (snapshot) {
+
         console.log("filtering", snapshot.val());
       });
  })
@@ -48,3 +67,38 @@
  $("#add-user-btn").click(function() {
 	window.location = 'matches.html';
 })
+
+
+if (localStorage.getItem("selectedCity")) {
+        firebaseAdded("city", "selectedCity");
+ }
+
+function firebaseAdded(parameter1, parameter2) {
+    database.ref().orderByChild(parameter1).equalTo(localStorage.getItem(parameter2)).on("child_added", function (snapshot) {
+
+    var tableRow=$("<tr>");
+    $("#tableBody").append(tableRow);
+ 
+    var tableName = $("<td>");
+    tableRow.append(tableName.text(snapshot.val().name));
+ 
+    var tableAge = $("<td>");
+    tableRow.append(tableAge.text(snapshot.val().age));
+ 
+    var tableGender = $("<td>");
+    tableRow.append(tableGender.text(snapshot.val().gender));
+ 
+    var tableCity = $("<td>");
+    tableRow.append(tableCity.text(snapshot.val().city));
+    
+    var beefRow = $("<tr>");
+    $("#tableBody").append(beefRow);
+
+    if(localStorage.getItem("selectedReligion", religion) !== database.ref().orderByChild("religion").once("value")) {
+    var tableReligion = $("<td>");
+    beefRow.append(tableReligion.text(snapshot.val().religion));
+    } else {
+    console.log("samsies")
+    }
+}
+)}
