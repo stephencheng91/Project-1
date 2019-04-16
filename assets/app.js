415f93f28f83e14758181cbb7f171bdeca5f041f
Nursaid Saidmakhmudov <uzblet@gmail.com>
2019-04-16 01:23:39 -0700

    map added to html

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,32 @@
 
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyDEU7u0sN9_gOCtEBz_J2VpUHbZ0zVmFpM",
-   authDomain: "plentyoftiffs.firebaseapp.com",
-   databaseURL: "https://plentyoftiffs.firebaseio.com",
-   projectId: "plentyoftiffs",
-   storageBucket: "plentyoftiffs.appspot.com",
-   messagingSenderId: "400267177365"
- };
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEU7u0sN9_gOCtEBz_J2VpUHbZ0zVmFpM",
+    authDomain: "plentyoftiffs.firebaseapp.com",
+    databaseURL: "https://plentyoftiffs.firebaseio.com",
+    projectId: "plentyoftiffs",
+    storageBucket: "plentyoftiffs.appspot.com",
+    messagingSenderId: "400267177365"
+};
 
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 
- var database = firebase.database();
+var database = firebase.database();
 
- var name = "";
- var age = 0;
- var city = "";
- var gender = "";
+var name = "";
+var age = 0;
+var city = "";
+var gender = "";
 
- $("#add-user-btn").click(function(event) {
-   
+$("#add-user-btn").click(function (event) {
+
     event.preventDefault();
-   
+
     name = $("#name-input").val();
     age = parseInt($("#age-input").val());
     city = $("#city-input").val();
-    if($("input[id=female-btn]:checked").val() === undefined) {
+    if ($("input[id=female-btn]:checked").val() === undefined) {
         gender = "male";
     } else {
         gender = "female"
@@ -39,8 +39,75 @@
         city: city,
         gender: gender
     })
- })
-
- $("#add-user-btn").click(function() {
-	window.location = 'matches.html';
 })
+
+$("#add-user-btn").click(function () {
+    window.location = 'matches.html';
+})
+
+database.ref().on("child_added", function (snapshot, prevChildKey) {
+
+    console.log(prevChildKey);
+
+    var childName = snapshot.val().name;
+    var childAge = snapshot.val().age;
+    var childGender = snapshot.val().gender;
+    var childCity = snapshot.val().city;
+
+    var tableRow = $("<tr>");
+    $("#tableBody").append(tableRow);
+
+    var tableName = $("<td>");
+    tableRow.append(tableName.text(childName));
+
+    var tableAge = $("<td>");
+    tableRow.append(tableAge.text(childAge));
+
+    var tableGender = $("<td>");
+    tableRow.append(tableGender.text(childGender));
+
+    var tableCity = $("<td>");
+    tableRow.append(tableCity.text(childCity));
+
+});
+
+
+
+
+var map, infoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here, Dude.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
